0979236bfbd0e7b43121ac264b4cf7b715d000a6
tighug <kapuriko123@gmail.com>
2020-08-21 02:54:59 +0900

    feat: Remove ListPage

--- src/renderer/containers/Root.tsx
+++ src/renderer/containers/Root.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { hot } from "react-hot-loader";
 import { Store } from "../store";
 import Routes from "./Routes";
-import { HashRouter, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 type RootProps = {
   store: Store;
@@ -13,8 +13,6 @@ function Root({ store }: RootProps) {
   return (
     <Provider store={store}>
       <HashRouter>
-        <Link to="/">Home</Link>
-        <Link to="/list">List</Link>
         <Routes />
       </HashRouter>
     </Provider>
